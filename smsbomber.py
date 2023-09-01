e2ffbabf4c7f1ec813e47d83451015b4f1125fca
omicron <131537767+omicr0nn@users.noreply.github.com>
2023-09-01 16:04:26 +0300

    Update smsbomber.py

--- smsbomber.py
+++ smsbomber.py
@@ -3,6 +3,33 @@ from time import sleep
 from os import system
 import urllib3
 import art
+import sys
+import time
+from os import system
+
+system("cls||clear")
+def rainbow_text(text, delay=0.1):
+    colors = [
+    "\033[31m", "\033[33m", "\033[32m", "\033[36m", "\033[34m", "\033[35m", "\033[37m",
+    ]
+
+    for i in range(len(text)):
+        char = text[i]
+        color = colors[i % len(colors)]
+        sys.stdout.write(color + char)
+        sys.stdout.flush()
+        time.sleep(delay)
+
+    sys.stdout.write("\033[0m")
+    sys.stdout.write("\n")
+
+def main():
+    rainbow_text("Coding by omicr0n")
+    sys.stdout.write("\033[37m")
+    sys.stdout.write("Coding by Omicron\n")
+	
+if __name__ == "__main__":
+    rainbow_text("Coding by omicr0n")
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
